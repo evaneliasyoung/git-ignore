76124da9a2e29419d6d04086b8bd3e49a120bda4
Evan Elias Young <evanyoung762@gmail.com>
2025-03-30 23:25:44 -0500

    build(exe): :heavy_plus_sign: add `clap`

--- build.zig
+++ build.zig
@@ -76,6 +76,9 @@ pub fn build(b: *std.Build) void {
     // file path. In this case, we set up `exe_mod` to import `lib_mod`.
     exe_mod.addImport("git_ignore_lib", lib_mod);
 
+    const clap = b.dependency("clap", .{});
+    exe_mod.addImport("clap", clap.module("clap"));
+
     // Now, we will create a static library based on the module we created above.
     // This creates a `std.Build.Step.Compile`, which is the build step responsible
     // for actually invoking the compiler.
