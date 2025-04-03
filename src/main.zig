a66170daddc7a3e1f66b56d66500517007cf7823
Evan Elias Young <evanyoung762@gmail.com>
2025-04-02 22:03:33 -0500

    feat(exe): :construction: add basic CLI

--- src/main.zig
+++ src/main.zig
@@ -1,46 +1,90 @@
-//! By convention, main.zig is where your main function lives in the case that
-//! you are building an executable. If you are making a library, the convention
-//! is to delete this file and start with root.zig instead.
+const main_params = clap.parseParamsComptime(
+    \\-l, --list    List templates
+    \\-u, --update  Update all templates by fetching them from gitignore.io
+    \\-w, --write   Write to .gitignore file instead of stdout
+    \\-f, --force   Forcefully overwrite existing .gitignore file
+    \\<string>
+    \\
+);
+
+pub const SubCommands = enum {
+    help,
+};
+
+pub const MainArguments = struct {
+    list: bool,
+    update: bool,
+    write: bool,
+    force: bool,
+};
 
 pub fn main() !void {
-    // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
+    var gpa_state = std.heap.GeneralPurposeAllocator(.{}){};
+    const gpa = gpa_state.allocator();
+    defer _ = gpa_state.deinit();
 
-    // stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    const stdout_file = std.io.getStdOut().writer();
-    var bw = std.io.bufferedWriter(stdout_file);
-    const stdout = bw.writer();
+    var c = Chameleon.initRuntime(.{ .allocator = gpa });
+    defer c.deinit();
 
-    try stdout.print("Run `zig build test` to run the tests.\n", .{});
+    var iter = try std.process.ArgIterator.initWithAllocator(gpa);
+    defer iter.deinit();
 
-    try bw.flush(); // Don't forget to flush!
-}
+    _ = iter.next();
 
-test "simple test" {
-    var list = std.ArrayList(i32).init(std.testing.allocator);
-    defer list.deinit(); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "use other module" {
-    try std.testing.expectEqual(@as(i32, 150), lib.add(100, 50));
-}
-
-test "fuzz example" {
-    const Context = struct {
-        fn testOne(context: @This(), input: []const u8) anyerror!void {
-            _ = context;
-            // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-            try std.testing.expect(!std.mem.eql(u8, "canyoufindme", input));
-        }
+    var diag = clap.Diagnostic{};
+    var res = clap.parseEx(clap.Help, &main_params, clap.parsers.default, &iter, .{
+        .diagnostic = &diag,
+        .allocator = gpa,
+        .terminating_positional = 0,
+    }) catch |err| {
+        diag.report(std.io.getStdErr().writer(), err) catch {};
+        return err;
     };
-    try std.testing.fuzz(Context{}, Context.testOne, .{});
+    defer res.deinit();
+
+    const main_args = MainArguments{
+        .list = res.args.list != 0,
+        .update = res.args.update != 0,
+        .write = res.args.write != 0,
+        .force = res.args.force != 0,
+    };
+
+    if (main_args.force and !main_args.write) {
+        defer std.process.exit(2);
+        try cli.print.err(&c, "--force can only be used with --write\n", .{});
+    }
+
+    var templates: std.ArrayListUnmanaged([]const u8) = .empty;
+    defer templates.deinit(gpa);
+
+    if (res.positionals[0]) |maybe_command| {
+        if (std.meta.stringToEnum(SubCommands, maybe_command)) |command| {
+            switch (command) {
+                .help => std.debug.print("TODO: remove once there are real commands (help)\n", .{}),
+            }
+        } else {
+            try templates.append(gpa, maybe_command);
+        }
+    }
+    while (iter.next()) |template| {
+        try templates.append(gpa, template);
+    }
+
+    // TODO: update logic
+
+    if (main_args.update and templates.items.len == 0) {
+        std.process.exit(0);
+    }
+
+    // TODO: writer logic
+
+    std.debug.print("{any}\n", .{templates});
 }
 
 const std = @import("std");
+const clap = @import("clap");
+const Chameleon = @import("chameleon");
 
-/// This imports the separate module containing `root.zig`. Take a look in `build.zig` for details.
+const cli = @import("cli/cli.zig");
+
 const lib = @import("git_ignore_lib");
