e28e762e85f43eec7a43c7f6c39dca3867eef48b
Evan Elias Young <evanyoung762@gmail.com>
2025-04-07 23:58:03 -0500

    feat(lib): :sparkles: add `IgnoreAliases`

--- src/root.zig
+++ src/root.zig
@@ -3,12 +3,14 @@ const testing = std.testing;
 
 pub const version = @import("build").version;
 
+pub const IgnoreAliases = @import("lib/ignore_aliases.zig");
 pub const IgnoreFile = @import("lib/ignore_file.zig");
 pub const IgnoreFiles = @import("lib/ignore_files.zig");
 pub const IgnoreSite = @import("lib/ignore_site.zig");
 pub const utils = @import("lib/utils.zig");
 
 test {
+    _ = @import("lib/ignore_aliases.zig");
     _ = @import("lib/ignore_file.zig");
     _ = @import("lib/ignore_files.zig");
     _ = @import("lib/ignore_site.zig");
